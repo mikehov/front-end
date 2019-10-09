5e24f356134d2b9aa6a96acc94357b0d8c98e785
Mike <mikehovje@gmail.com>
2019-10-09 11:55:20 +0200

    Versie weet ik niet
    Versie weet ik niet

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//******** UTILS.JS CHEATSHEET BY MIKE HOVENIER ********//
+
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+/*eslint-env browser*/
+/*eslint 'no-console': 0*/
+
+function hamburger() {
+    document.querySelector("nav ul") .classList.toggle("popnav");
+    console.log('Navigatie klik');
+}
+
+document.querySelector(".hamburger").addEventListener("click", hamburger);
+
+
+
+
+
+console.log('Script werkt');
