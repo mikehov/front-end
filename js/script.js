1e4ca80aa985c148d20d859ed149bb22b451640c
Mike <mikehovje@gmail.com>
2019-10-27 13:25:24 +0100

    Last version - beoordeling (1) - 27 oktober 2019
    Laatste beoordeling voor eerste oplevering.
    27 oktober 2019

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@
 /*eslint-env browser*/
 /*eslint 'no-console': 0*/
 
+console.log('Script werkt.');
+
+
 function hamburger() {
     document.querySelector("nav ul").classList.toggle("popnav");
     console.log('Navigatie klik');
@@ -12,4 +15,13 @@ function hamburger() {
 document.querySelector(".hamburger").addEventListener("click", hamburger);
 
 
-console.log('Script werkt');
+function filterFunctie() {
+    document.querySelector(".filterknop .filter").classList.toggle("filteractive");
+    document.querySelector("");
+    console.log('button klik');
+}
+
+document.querySelector(".filterknop button").addEventListener("click", filterFunctie);
+
+
+
